221ea54a06e92e16fca3493579af153fc8e6785e
Biff <thedonnieutley@gmail.com>
2017-11-01 13:44:53 -0500

    initial commit & working

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,9 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    const friends = axios.get('http://10.0.0.95:5000/friends');
+    return {
+        type: GET_FRIENDS,
+        payload: friends
+    };
 };
